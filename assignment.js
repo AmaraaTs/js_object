3a2050f6f1a2099d06e1abf83eb808a574ddb916
AmaraaTs <amarjargal.ts01@gmail.com>
2024-06-18 12:27:01 +0800

    hw barag duuslaadaa hah

--- assignment.js
+++ assignment.js
@@ -1,66 +1,122 @@
-// bodlogo1
+// bodlogo1. Өгөгдсөн 2 тооны ихийг олох функц бич.
 
-let a1 = 10;
-let b1 = 20;
-
-function bod1() {
-  if (a1 > b1) {
-    console.log("Bodlogo1: ", a1);
+function bod1(x1, y1) {
+  if (x1 > y1) {
+    return x1;
   } else {
-    console.log("Bodlogo1: ", b1);
+    return y1;
   }
 }
-bod1();
+console.log("Bodlogo1: ", bod1(15, 16));
 
-// bodlogo2
-
-let a2 = 10;
-let b2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let c2 = -1;
-
-for (i = 0; i < b2.length; i++) {
-  if (a2 === b2[i]) {
-    c2 = i;
-  }
-}
-console.log("Bodlogo2: ", c2);
-
-// check
+// bodlogo2. Өгөгдсөн array -аас өгөгдсөн тоо байгаа эсэхийг олох функц бич. Тухайн тоо байвал индексийг буцаана. Байхгүй бол -1 буцаана.
 
 let array = [10, 20, 30, 40, 50];
-function search(x) {
-  for (i = 0; i < array.length; i++) {
-    if (x === array[i]) {
+
+function findIndex(searchArr, findToo) {
+  console.log("Bodlogo2: Array: ", array);
+  console.log("Bodlogo2: Find number: ", findToo);
+  for (let i = 0; i < searchArr.length; i++) {
+    if (searchArr[i] === findToo) {
       return i;
-    } else {
-      -1;
     }
   }
+  return -1;
+}
+let idx = findIndex(array, 40);
+
+console.log("Bodlogo2: index: ", idx);
+
+// bodlogo3. Өгөгдсөн тэмдэгт мөрөөс -аас өгөгдсөн тэмдэгт байгаа эсэхийг олох функц бич. Тухайн тэмдэгт байвал индексийг буцаана. Байхгүй бол -1 буцаана.
+
+let array3 = ["айыбөй", "айыбөй", "айыбөйаас", "айыбөй", "айыбөй"];
+
+function findAas(searchArr, findText) {
+  console.log("Bodlogo3: Array: ", array3);
+  console.log("Bodlogo3: Find number: ", findText);
+  for (i = 0; i < searchArr.length; i++) {
+    if (searchArr[i].indexOf(findText) !== -1) {
+      return i;
+    }
+  }
+  return -1;
+}
+let text3 = findAas(array3, "аас");
+console.log("Bodlogo3: ", text3);
+
+// bodlogo4. Өгөгдсөн array - ийн дундажыг олох функц бич.
+
+let array4 = [10, 20, 30, 40, 50];
+
+function avg(avgArray) {
+  let s = 0;
+  for (let i = 0; i < avgArray.length; i++) {
+    s += avgArray[i];
+  }
+
+  return s / avgArray.length;
 }
 
-console.log("Array x:", search(60));
+let av = sum([8934, 34, 3, 2]);
+console.log("Bodlogo4: ", av);
+let av1 = avg([8934, 34, 3, 2]);
+console.log("Bodlogo4: ", avg([8934, 34, 3, 2]));
 
-// bodlogo3
+// bodlogo5. Өгөгдсөн array - ийн нийлбэрийг олох функц бич.
 
-let a3 = "аас";
-let b3 = ["айыбөй", "айыбөй", "айыбөй", "айыбөйаас", "айыбөй"];
-let c3 = -1;
+function sum(sumArray) {
+  let s = 0;
+  for (let i = 0; i < sumArray.length; i++) {
+    s += sumArray[i];
+  }
+  return s;
+}
+let summ = sum(array4);
+console.log("Bodlogo5: ", summ);
 
-for (i = 0; i < b3.length; i++) {
-  if (b3[i].indexOf(a3) !== -1) {
-    c3 = i;
+// bodlogo6. Өгөгдсөн тоо анхны эсэхийг олох функц бич.
+
+function num(x) {
+  for (let i = 2; i < x; i++) {
+    if (x % i === 0) {
+      return "Анхны тоо биш";
+    }
+  }
+  return "Анхны тоо";
+}
+let aToo = num(11);
+console.log("Bodlogo6: ", aToo);
+
+// bodlogo7. Palindrome too eseh jishee ni: input: 121 output: true, input: -121 output: false, input: 12  output: false
+
+function pali(x) {
+  let y = 0;
+  let z = 0;
+  let u = x;
+  while (x !== 0) {
+    y = x % 10;
+    x = x - y;
+    x = x / 10;
+    z = z * 10 + y;
+  }
+  if (u === z) {
+    return "Палиндром";
+  } else {
+    return "Палиндром биш";
   }
 }
-console.log("Bodlogo3: ", c3);
+let palindrom = pali(1234321);
+console.log("Bodlogo7: ", palindrom);
 
-// bodlogo 4
+// library - math object
+let mm = Math.max(10, 12, 15, 4);
+console.log("Max turshilt: ", mm);
 
-let a4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let c4 = 0;
-// let b4 = (a4) => {
-//   for (i = 0; 0 < a4.length; i++) {
-//     c4 = c4 + a4[i];
-//   }
-// };
-function dundaj(a4[]) 
-b4();
+// math.floor - dooshoo bvhelcildeg
+// math.ceil - deeshee bvhelcildeg
+// math.round - toimloh
+
+let random = Math.random(); // 0-1 hoorond random too gargah
+console.log(random);
+
+// bodlog8. Ugugdsun string urvuulna gargah jishee ni: input: hello output: olleh
